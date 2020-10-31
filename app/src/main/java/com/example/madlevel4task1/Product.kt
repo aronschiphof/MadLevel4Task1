b52d7e1af74c0923ad67e7cfbdbb54b7c8665cff
Aron Schiphof <aron.schiphof@gmail.com>
2020-11-01 00:44:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/madlevel4task1/Product.kt
@@ -0,0 +1,20 @@
+package com.example.madlevel4task1
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product_table")
+data class Product(
+
+    @ColumnInfo(name = "product")
+    var name: String,
+
+    @ColumnInfo(name = "quantity")
+    val quantity: Int,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+
+)
---
+++ app/src/main/java/com/example/madlevel4task1/ProductDAO.kt
@@ -0,0 +1,20 @@
+package com.example.madlevel4task1
+
+import androidx.room.*
+
+@Dao
+interface ProductDao {
+
+    @Query("SELECT * FROM product_table")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM product_table")
+    suspend fun deleteAllProducts()
+
+}
